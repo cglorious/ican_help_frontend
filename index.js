9854f107cfcf88a1b27503a0c3b4da539bbcab7d
Catherine Gloria <catherinegloria@gmail.com>
2021-02-21 11:24:49 -0800

    render chart name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const BASE_URL = "http://localhost:3000"
 const CHARTS_URL = `${BASE_URL}/api/v1/charts`
 const JOBS_URL = `${BASE_URL}/api/v1/jobs`
-const body = document.querySelector('body')
+const main = document.querySelector('body')
 
 document.addEventListener('DOMContentLoaded', () => {
   loadCharts()
@@ -16,19 +16,7 @@ function loadCharts(){
 }
 
 function renderChart(chart) {
-  // const div = document.createElement('div')
   const p = document.createElement('p')
-  // const btn = document.createElement('button')
-  // const ul = document.createElement('ul')
-  //
-  // div.setAttribute('class', 'card')
-  // div.setAttribute('data-id', trainer.id)
-  // btn.setAttribute('data-trainer-id', trainer.id)
-
   p.innerText = chart.attributes.name
-  // btn.innerText = "Add Pokemon"
-  // btn.addEventListener("click", addPokemon)
-
-  // div.append(p, btn, ul)
-  body.append(p)
+  main.append(p)
 }
