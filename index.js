71700e5ab2513a97e276ab8e3d9fbe34f0c974e8
Catherine Gloria <catherinegloria@gmail.com>
2021-02-21 11:03:52 -0800

    dom content loaded, first fetch request

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("in index.js")
+const BASE_URL = "http://localhost:3000"
+const CHARTS_URL = `${BASE_URL}/api/v1/charts`
+const JOBS_URL = `${BASE_URL}/api/v1/jobs`
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadCharts()
+})
+
+function loadCharts(){
+  fetch(CHARTS_URL)
+  .then(resp => resp.json())
+  .then(chart => {
+    console.log(chart);
+  })
+}
